be3377077489642203c59b81a926d6a02538a1a0
pfirulin <pfirulin@yandex.ru>
2022-09-24 03:44:16 +0800

    burger just ready

--- script/script.js
+++ script/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
     $('.slider').slick({
         // dots:true,
+        // arrows: false;
+        responsive: [{
+            breakpoint: 500,
+            settings: {
+                arrows : false,
+                dots:true,
+            }
+        }, ]
+    });
+
+    $('.kitchen').click(function (event)
+    {
+        $('.kitchen,menu').toggleClass('active');
     });
 })
 
